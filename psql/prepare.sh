51f122d65178e679518ab0d02a679f1a01cf5a01
Vladimir Belous <white.anonymous777@gmail.com>
2022-10-04 00:26:11 +0300

    fix sh && add restart policy

--- psql/prepare.sh
+++ psql/prepare.sh
@@ -5,4 +5,12 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER fluent WITH PASSWORD 'admin';
     CREATE DATABASE fluentbit;
     GRANT ALL PRIVILEGES ON DATABASE fluentbit TO fluent;
+    \c fluentbit
+    set role fluent;
+    CREATE TABLE log
+            (
+                tag varchar,
+                time timestamp,
+                data jsonb
+            );
 EOSQL
---
+++ readme.md
@@ -0,0 +1,8 @@
+1. For build docker image use:
+- docker-compose build
+2. For start project ci/cd
+- docker-compose up -d
+----------
+credentials db:
+log: fluent
+pass: admin
